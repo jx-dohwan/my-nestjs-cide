1e4ccd62e0caa33829a4212af9d0ebd3780dac15
jx-dohwan <jx7789@gmail.com>
2025-10-06 16:19:30 +0900

    FEAT 테스트2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! cicd 테스트';
+    return 'Hello World! cicd 테스트2';
   }
 }
