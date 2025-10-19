92a686954bfdaa8263f5a6c7b546c3fa9435df2f
jx-dohwan <jx7789@gmail.com>
2025-10-19 22:15:44 +0900

    [FEAT] version 4.0

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from ECS! Version 3.0';
+    return 'Hello World from ECS! Version 4.0';
   }
 }
